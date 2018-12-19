7a57e85427f605b7c50f76c41bc7b45f5750e63a
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-19 15:24:47 +0100

    Add click event to button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 const jokeUrl = 'http://api.icndb.com/jokes/random';
 
 let jokeButton = document.getElementById("joke-button");
-let resultParagraph = document.getElementById("joke-button");
+let resultParagraph = document.getElementById("joke-text");
 
 function getJoke() {
 	let request = new XMLHttpRequest();
-	request.open("GET", jokeurl);
+	request.open("GET", jokeUrl);
 	request.addEventListener("load", function() {
 		let response = JSON.parse(request.response);
 		resultParagraph.innerHTML = response.value.joke;
 	});
 	request.send();
 }
+
+jokeButton.addEventListener("click", function() {
+	getJoke();
+});
+
+getJoke();

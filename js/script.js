4314385a266fbe66be9cd669e0be1415a3517b07
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-19 15:16:17 +0100

    Add get joke function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 const jokeUrl = 'http://api.icndb.com/jokes/random';
 
 let jokeButton = document.getElementById("joke-button");
-let resultParagraph = document.getElementById("joke-button")
+let resultParagraph = document.getElementById("joke-button");
+
+function getJoke() {
+	let request = new XMLHttpRequest();
+	request.open("GET", jokeurl);
+	request.addEventListener("load", function() {
+		let response = JSON.parse(request.response);
+		resultParagraph.innerHTML = response.value.joke;
+	});
+	request.send();
+}
